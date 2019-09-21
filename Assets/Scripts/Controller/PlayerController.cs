7e9f1b5e26699560de3260fc9b1690a4c1d2c07d
Tyrease Teer <kayaneseera@gmail.com>
2019-09-20 22:45:11 -0600

    Going Good!!!

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    #region Public Members
+    //Have our keys mapped;
+    public KeyCode left = KeyCode.LeftArrow;
+    public KeyCode right = KeyCode.RightArrow;
+    public KeyCode up = KeyCode.UpArrow;
+    public KeyCode down = KeyCode.DownArrow;
+    public KeyCode shoot = KeyCode.Z;
+    public KeyCode special1 = KeyCode.A;
+    public KeyCode special2 = KeyCode.S;
+    public KeyCode special3 = KeyCode.D;
+    #endregion
+
+    #region Private Members
+    //Reference Pawn
+    PlayerPawn pawn;
+    #endregion
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        pawn = GetComponent<PlayerPawn>();
     }
 
     // Update is called once per frame
@@ -15,4 +33,32 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    //This will be based on rotating, so we need a rotating function
+    public void Rotate(object _direction)
+    {
+        //A switch case of if it's "Left" or "Right"
+        switch (_direction)
+        {
+            //It takes Left and Right as 
+            case "Left":
+
+                break;
+            case "Right":
+
+                break;
+
+            case -1:
+
+                break;
+            case 1:
+
+                break;
+            default:
+                //Do Nothing
+                break;
+        }
+    }
+
+    //Remember, we are controlling pawn!!!
 }
--- Assets/Scripts/Pawn/PlayerPawn.cs
+++ Assets/Scripts/Pawn/PlayerPawn.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerPawn : MonoBehaviour
 {
+    //We'll need our Transform
+    Transform pawnTransform;
+
+    //We'll add the important stuff
+    Vector2 startPoint;
+    float g_angle = 0f;
+    float radius = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pawnTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //We do this to eliminate the risk of overflowing
+        if (Mathf.Abs(g_angle) > 359) g_angle = 0; 
+
+
+    }
+
+    //We'll get 2 functions, MoveInCircle, and MoveOnDiameter
+    //Either circle around Luu, or go towards her.
+    void MoveInCircle(float _speed)
+    {
+        float angleStep = 360f / _speed;
+        float angle = g_angle;
+        gameObject.transform.Translate((new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * radius) * _speed);
+
+        angle += angleStep;
+    }
+
+    void MoveOnDiameter(float _speed, Transform _target)
+    {
+        //This will calculate the vector between Raven and Luu, and will move on that normalized vector
+        Vector2 playerToLuu = _target.position - pawnTransform.position;
+        gameObject.transform.TransformVector(playerToLuu);
     }
 }
